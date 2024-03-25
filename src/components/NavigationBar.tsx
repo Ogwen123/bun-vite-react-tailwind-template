166693fb1102cc37178fe1145c1bd54662f9a4f8
Ogwen123 <owen.d.jones@btinternet.com>
2024-03-25 17:43:35 +0000

    Update NavigationBar.tsx

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,11 +2,8 @@ import React from 'react'
 
 const NavigationBar = () => {
     return (
-        <div className='flex items-center px-[20px] h-[10vh] bg-bgdark text-xl'>
-            <div className='bg-gradient-to-r from-main to-[#f57c00] text-transparent bg-clip-text text-5xl h-full flex items-center'>Ogwen.eu.org</div>
-            <a href="http://github.com/Ogwen123" target='_blank'>
-                Github
-            </a>
+        <div className='fc px-[20px] h-[10vh] bg-bgdark text-xl'>
+            Navigation Bar
         </div>
     )
 }
